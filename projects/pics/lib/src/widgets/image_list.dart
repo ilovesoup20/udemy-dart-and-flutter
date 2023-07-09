84c9302737cf0071c41d37142eb71b70b15d670a
Charles Chong <ilovesoup@fastmail.com>
2023-07-09 14:36:25 +0900

    Update

--- projects/pics/lib/src/widgets/image_list.dart
+++ projects/pics/lib/src/widgets/image_list.dart
@@ -5,15 +5,32 @@ class ImageList extends StatelessWidget {
   final List<ImageModel> images;
   const ImageList(this.images, {super.key});
 
+  @override
   Widget build(ctx) {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Container(
-          margin: const EdgeInsets.all(20.0),
-          child: Image.network(images[index].url),
-        );
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      margin: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Image.network(image.url),
+          ),
+          Text(image.title),
+        ],
+      ),
     );
   }
 }
